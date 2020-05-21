fff5ca6bfeca5d9239ac96c4bd2c40a2f558d542
CzLeri <v.leroch@gmail.com>
2020-05-21 18:57:07 +0200

    My first program in WPF

---
+++ Switch/Switch/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using IPSwitch;
+using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Switch
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string ipAddress;
+        private string subnet;
+        bool checkboxDHCP;
+        bool Running = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            afterStartApp();
+        }
+
+        private void btnActivate_Click(object sender, RoutedEventArgs e)
+        {
+            if (chkDHCP.IsChecked == true)
+            {
+                NetAdapt.SetDHCP(cboNIC.SelectedItem.ToString());
+            }
+            else
+            {
+                if(checkSyntax(txIP.Text) && checkSyntax(txSubnet.Text))
+                    NetAdapt.SetIP(txIP.Text, txSubnet.Text, cboNIC.SelectedItem.ToString());
+            }
+        }
+        private void afterStartApp()
+        {
+            
+
+            ArrayList nicNames = NetAdapt.GetNICNames();
+            cboNIC.Items.Clear();
+            foreach (string nic in nicNames)
+            {
+                cboNIC.Items.Add(nic);
+            }
+            if (cboNIC.Items.Count > 0)
+            {
+                cboNIC.SelectedIndex = 0;
+                ipAddress = NetAdapt.GetIp(cboNIC.Text);
+                txIP.Text = ipAddress;
+                subnet = NetAdapt.GetMask(cboNIC.Text);
+                txSubnet.Text = subnet;
+                checkboxDHCP = NetAdapt.CheckDHCP(cboNIC.Text);
+                chkDHCP.IsChecked = checkboxDHCP;
+
+                
+                Running = true;
+            }
+
+               
+        }
+
+        private void cboNIC_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Running)
+            {
+                
+                ipAddress = NetAdapt.GetIp(cboNIC.SelectedItem.ToString());
+                txIP.Text = ipAddress;
+                subnet = NetAdapt.GetMask(cboNIC.SelectedItem.ToString());
+                txSubnet.Text = subnet;
+                checkboxDHCP = NetAdapt.CheckDHCP(cboNIC.SelectedItem.ToString());
+                chkDHCP.IsChecked = checkboxDHCP;
+                if (checkboxDHCP)
+                {
+                    txIP.IsEnabled = false;
+                    txSubnet.IsEnabled = false;
+                }
+                else
+                {
+                    txIP.IsEnabled = true;
+                    txSubnet.IsEnabled = true;
+                }
+                
+                
+            }
+            
+        }
+
+        private void chkDHCP_Click(object sender, RoutedEventArgs e)
+        {
+            if(chkDHCP.IsChecked==true)
+            {
+                txIP.IsEnabled = false;
+                txSubnet.IsEnabled = false;
+            }
+            else
+            {
+                txIP.IsEnabled = true;
+                txSubnet.IsEnabled = true;
+            }
+            
+        }
+        private bool checkSyntax (string text)
+        {
+            bool okFormat = false;
+            string[] data = text.Split('.');
+            int[] pomoc = new int[4];
+            for(int i = 0; i < 4;i++)
+            {
+                
+                try
+                {
+                    pomoc[i] = int.Parse(data[i]);
+                    if (pomoc[i] >= 0 && pomoc[i] <= 255)
+                        okFormat = true;
+                    else
+                    {
+                        okFormat = false;
+                        break;
+                    }
+                }
+                catch
+                {
+                    okFormat = false;
+                }                                         
+           }
+            return okFormat;
+        }
+    }
+}
---
+++ Switch/Switch/NetAdapt.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Management;
+
+namespace IPSwitch
+{
+    class NetAdapt
+    {
+        public static ArrayList GetNICNames()
+        {
+            ArrayList nicNames = new ArrayList();
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject m in moc)
+            {
+                if ((bool)m["ipEnabled"])
+                {
+                    nicNames.Add(m["caption"]);
+                }
+            }
+            return nicNames;
+        }
+
+        public static string GetIp(string nicName)
+        {
+            string[] ipaddresses = null;
+            string ipAddress = "";
+            //subnets = null;
+
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach(ManagementObject m in moc)
+            {
+                if((bool)m["ipEnabled"])
+                {
+                    if(m["caption"].Equals(nicName))
+                    {
+                        ipaddresses = (string[]) m["IPAddress"];
+                        //subnets = (string[])m["IPSubnet"];
+                        break;
+                    }
+                }
+                
+            }
+            ipAddress = ipaddresses[0];
+            return ipAddress;
+        }
+
+        public static string GetMask(string nicName)
+        {
+            string[] subnets = null;
+            string subnet = "";
+            //subnets = null;
+
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject m in moc)
+            {
+                if ((bool)m["ipEnabled"])
+                {
+                    if (m["caption"].Equals(nicName))
+                    {
+                        subnets = (string[])m["IPSubnet"];
+                        break;
+                    }
+                }
+
+            }
+            subnet = subnets[0];
+            return subnet;
+        }
+
+        public static bool CheckDHCP(string nicName)
+        {
+            bool checkDH = false;
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach(ManagementObject m in moc)
+            {
+               
+                    if(m["caption"].Equals(nicName))
+                    {
+                        if ((bool)m["DHCPEnabled"])
+                        {
+                            checkDH = true;
+                            break;
+                        }
+                    }
+                
+            }
+            return checkDH;
+        }
+        public static void SetDHCP(string nicName)
+        {
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach(ManagementObject m in moc)
+            {
+                if(m["caption"].Equals(nicName))
+                {
+                    ManagementBaseObject newDns = m.GetMethodParameters("SetDNSServerSearchOrder");
+                    newDns["DNSServerSearchOrder"] = null;
+                    ManagementBaseObject enableDHCP = m.InvokeMethod("EnableDHCP", null, null);
+                    ManagementBaseObject setDNS = m.InvokeMethod("SetDNSServerSearchOrder", newDns, null);
+                    break;
+                }
+            }
+        }
+        public static void SetIP(string ipAddress, string subNet, string nicName)
+        {
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject m in moc)
+            {
+                if(m["caption"].Equals(nicName))
+                {
+                    ManagementBaseObject newIP = m.GetMethodParameters("EnableStatic");
+                    ManagementBaseObject newGate = m.GetMethodParameters("SetGateways");
+                    ManagementBaseObject newDNS = m.GetMethodParameters("SetDNSServerSearchOrder");
+
+                    newIP["IPAddress"] = ipAddress.Split(',');
+                    newIP["SubnetMask"] = new string[] { subNet };
+
+                    ManagementBaseObject setIP = m.InvokeMethod("EnableStatic", newIP, null);
+                    ManagementBaseObject setGateways = m.InvokeMethod("SetGateways", newGate, null);
+                    ManagementBaseObject setDNS = m.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+
+                    break;
+                }
+            }
+        }
+    }
+}
